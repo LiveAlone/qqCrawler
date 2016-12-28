1a71e928b0015740665f58c5faafc8bfb7c9681b
yaoqijun <yaoqijun@changingedu.com>
2016-12-28 19:39:16 +0800

    add all

--- src/main/java/org/qingqing/crawler/demo/single/SingleMainTest.java
+++ src/main/java/org/qingqing/crawler/demo/single/SingleMainTest.java
@@ -34,13 +34,15 @@ public class SingleMainTest {
 
     private static String xpPassword = "";
 
-    private static String basicUrl = "http://search.zxxk.com/Search1.aspx?keyword=&typeid=1&provinceid=4&year=2016&isfree=1&orderby=score&pagesize=10&isprecise=0&onlytitle=1&dontsave=0&SelectTypeID=3&page=";
+//    private static String basicUrl = "http://search.zxxk.com/Search1.aspx?keyword=&typeid=1&provinceid=4&year=2016&isfree=1&orderby=score&pagesize=10&isprecise=0&onlytitle=1&dontsave=0&SelectTypeID=3&page=";
+
+    private static String basicUrl =   "http://search.zxxk.com/Search1.aspx?keyword=&typeid=1&provinceid=4&year=2017&ismoney=1&orderby=score&pagesize=10&isprecise=0&onlytitle=1&dontsave=0&SelectTypeID=3&page=";
 
     private static Integer totalPage = 30;
 
     private static String logFile = "loadLog.txt";
 
-    private static String basicDir = "free/";
+    private static String basicDir = "money/";
 
     private static Integer page = 0;
 
@@ -56,6 +58,35 @@ public class SingleMainTest {
     }
 
     public static void main(String[] args) throws Exception{
+//        originalLoadContent();
+        targetFileLoad();
+    }
+
+    public static void originalLoadContent() throws Exception{
+        FileWriter fileWriter = new FileWriter(new File(logFile), true);
+        for (int i = 1; i<=6; i++){
+
+            String realUrl = basicUrl + i;
+            page = i;
+            List<String> result = analysePagedAnalyseUrl(getPageContent(realUrl));
+            fileWriter.append("current page is " + page + "\n");
+            for (String s : result) {
+                try {
+                    String basicUrlConvert = convertUrl(s);
+                    getResource(analyseDownloadPageContent(getPageContent(basicUrlConvert)));
+                    fileWriter.append("SUCCESS " + s + "\n");
+                    fileWriter.flush();
+                    Thread.sleep(5000);
+                }catch (Exception e){
+                    fileWriter.append("ERROR "+ s + "\n");
+                    fileWriter.flush();
+                }
+            }
+        }
+        fileWriter.close();
+    }
+
+    private static void targetFileLoad() throws Exception{
         FileWriter fileWriter = new FileWriter(new File(logFile), true);
         BufferedReader bufferedReader = new BufferedReader(new FileReader(new File("target.txt")));
 
@@ -68,10 +99,11 @@ public class SingleMainTest {
             Boolean status = false;
             String basicUrl = "";
             if (current.contains("current")){
-                page = Integer.valueOf(current.substring(current.length()-2, current.length()));
-                if ( page != 16)
+                page = Integer.valueOf(current.substring(current.length()-1, current.length()));
+                if (page !=2 && page!=3 && page!=4 && page!=5 && page!=6)
                     break;
                 fileWriter.append("current page is " + page + "\n");
+                fileWriter.flush();
                 continue;
             }else {
                 if (current.contains("ERROR")){
@@ -85,6 +117,7 @@ public class SingleMainTest {
 
             if (status){
                 fileWriter.append("JUMP " + basicUrl + "\n");
+                fileWriter.flush();
                 continue;
             }
 
@@ -95,34 +128,13 @@ public class SingleMainTest {
                 Thread.sleep(10000);
             }catch (Exception e){
                 fileWriter.append("ERROR "+ basicUrlConvert + "\n");
+                throw e;
             }
-
+            fileWriter.flush();
         }
 
         fileWriter.close();
         bufferedReader.close();
-    }
-
-    public static void originalLoadContent() throws Exception{
-//        FileWriter fileWriter = new FileWriter(new File(logFile), true);
-//        for (int i = 1; i<=30; i++){
-//
-//            String realUrl = basicUrl + i;
-//            page = i;
-//            List<String> result = analysePagedAnalyseUrl(getPageContent(realUrl));
-//            fileWriter.append("current page is " + page + "\n");
-//            for (String s : result) {
-//                try {
-//                    getResource(analyseDownloadPageContent(getPageContent(s)));
-//                    fileWriter.append("SUCCESS " + s + "\n");
-//                    Thread.sleep(5000);
-//                }catch (Exception e){
-//                    fileWriter.append("ERROR "+ s + "\n");
-//                }
-//            }
-//        }
-//        fileWriter.close();
-//        System.out.println("测试环境的配置方式");
     }
 
     private static String convertUrl(String originUrl){
@@ -133,18 +145,19 @@ public class SingleMainTest {
                 || originUrl.contains("http://dl.zxxk.com/soft/")
                 || originUrl.contains("http://zz.zxxk.com/soft/")
                 || originUrl.contains("http://hx.zxxk.com/soft/")
-                || originUrl.contains("http://ls.zxxk.com/soft/")){
+                || originUrl.contains("http://ls.zxxk.com/soft/")
+                || originUrl.contains("http://xx.zxxk.com/soft/")){
             return originUrl.replaceFirst("soft/", "s");
         }
 
-        return null;
+        return originUrl;
     }
 
     private static void getResource(String resourceUrl) throws Exception{
         HttpGet httpGet = new HttpGet(resourceUrl);
         CookiesManager cookiesManager = new CookiesManager();
         // first must content
-        cookiesManager.setAttribute("xk.passport","894AB9750164045D7CC481DD7AE294D403083A93CDFDA5ECA9C68DFCF17810B3AC30155AB2E8BF286B650E48766587316C249640F019AA19CF51A7AC7875CA53D66C2D496A66F2E1C1C30F558B5A94E5D0A6520BF2BE6C0E7744BE0863132C486119844793275C881B1D32CDDFB3F975E1D1DD3EE76BABC79E7262ADE467175A72812C4D003A83C6A523660B17B19AA910A3967EEA62A0C81BBEE238D26818B45288D1B214CD70B14203D0E75148B2F2957B882592750120BE26F640475009EF21C601C557D2AE413195E5E56C104470D8215C0AA4E135D5EEDFB843CAEABA884E058300060D6868B22DA5A445470B7A0ECD18AA2B8A797348443DE15592D710906D6D6A052BBF1386AB36ADDED1C73CC6DD95C55E4754644DF8E52611FF4E0EE60E69FBC7BDB7D46AC1BC26AB804B7E31A3D2B623149606A5B44CB25E7251A516CFAA1C7381EB4103F37CC5579F7915FE46CD24AD07F0E4B607A68C72DFA060EAB9A663DD05B32FCADD4031F12709122343C62397176A4B58A75DE11EEF35C5972B2BA7");
+        cookiesManager.setAttribute("xk.passport","EB821A6BA4518CC0549155B88998CBD3DEB96BD0A464D8ACADE3F70F30544D9D70961E019962FA17E4B32B474C6E9A8F83B978ADA2E65C1C1326FAE5AC7B8390A3C3A0C1B70CBDE0AD98BF6FE4C60467FF67DF3AB30FEFDF60D992C9252E41C63983A1EDE32AD6EA3C264B92AFCAD4D16BD79E64CA3CBEAE2041785083C727D97D5C10D6E5417C404B8508AA91B98835D5EE948832FD956D2FEC0F8208C35513F04E26034187C0F2507B80D0B94D2632AAC07F094768A068E9C0B9FBD31E5A73BD29290EE266530E727FBE8C86FA2F42FB607072C123E5F0C8DD921F7E40D623BE185F10DAA4EA4FC1EE29F9C0FC598F482037BA83E2AD0D09F6D23C6850D93C584DBE7DD724E3C9EC5500CB52FB35CC870D67652A942526750882B59474D85092D988537C6CAEC7DB89AB10FCEABDDB7A202E984B5A397EEE165F5E7D86844AE5EC8EBB201208C9FA33D1D09B745848C7F57A125EE5D9CF594CE3AC3E6DC38FCB7297616D4B5AE4EDAEE9C8B8DD6928DF8EC8FE27B844CA2033C0539E727F0E7B649A08");
 //        cookiesManager.setAttribute("xk.passport", "0088908718045B5321723848923CE0254AC2B9B5A4B9EC7D309F08F23524C04266304FC202BC66811D3626B539FC39B954897AE8555DAC4C699F3FE0EA5174EFF45B51A3188F62DB014E29B491DA187CE19072C9DBF73ABDD040E156AFB931487211565C6D1B1F859ACAF663A94291FD785C566A08D787852A076DE3310D8BDD1E05D963768ADB6FBF6FB32ADE9D3D3CDE279EC7E7252B4D248755321A0927B7DA0835EADE76C721E81819C67D1DE2108494C906E1217E49A6E2BF9B378C38B5ED42BF07CB16584F39892228E273806911EEC1FEE6732CB28B53AE6A08CD8F98A5DB5EA65CC4397EEBBC2D9D087A6D30C09E6E9376441623266B91393FCC5A13AC2D4FC57158E397F1A1F856FD1223B3DC927F9E98C1DF79B95F5D51737B91D5E60CB2F026E053A1E21BF9AE5536699A0245B17319EC163829EA017DA350AAA91C3F08A2E6530BF797004D940CFB208AD6B8C4291D305D8EF9F778360DF7D8402288AFEE042FF1E86D91639764AAB6B5809F1C62345FC0C789554160EEDD06DEE63CCDFB");
 //        cookiesManager.setAttribute("Hm_lvt_0e522924b4bbb2ce3f663e505b2f1f9c","1482582411");
 //        cookiesManager.setAttribute("Hm_lpvt_0e522924b4bbb2ce3f663e505b2f1f9c","1482585339");
@@ -159,6 +172,7 @@ public class SingleMainTest {
         String filename = URLDecoder.decode(filenameSource);
 
         System.out.println(filename);
+        filename = filename.replace(":", "-");
         File file = new File(basicDir + page + "/" + filename);
         Files.createParentDirs(file);
 
