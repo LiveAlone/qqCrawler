d48ca22ea39c404fa55a6631adc01b00f774a446
yaoqijun <yaoqijun@changingedu.com>
2017-02-17 18:42:27 +0800

    fix all

--- src/test/java/org/qingqing/crawler/demo/TestContent.java
+++ src/test/java/org/qingqing/crawler/demo/TestContent.java
@@ -1,5 +1,11 @@
 package org.yqj.boot.demo;
 
+import com.google.common.collect.Lists;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * Created by yaoqijun.
  * Date:2016-02-05
@@ -8,6 +14,13 @@ package org.yqj.boot.demo;
  */
 public class TestContent {
     public static void main(String[] args) {
-        System.out.println("test");
+        List<Integer> test = Lists.newArrayList(2, 3, 5, 5, 23, 41, 23, 123, 1);
+        Collections.sort(test, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2-o1;
+            }
+        });
+        System.out.println(test);
     }
 }
